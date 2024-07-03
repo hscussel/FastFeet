78ab50a45e9c9f38f069b1f8c51c3ebeb0cb82a4
Henrique Scussel <hscussel@hotmail.com>
2024-07-03 19:40:28 -0300

    final commit

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -1,20 +1,17 @@
 const express = require('express');
-const path = require('path');
-const adminRoutes = require('./routes/adminRoutes'); 
+
+
+
+const routes = require('./routes');
+
 const app = express();
-const port = 3000;
 
-app.use(express.static(path.join(__dirname, '../../../public')));
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/admin', adminRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../FrontEnd/index.html'));
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.use(routes);
+
+const PORT = process.env.APP_PORT || 3333;
+app.listen(PORT, () => console.log(`Server is runing on Port ${PORT}`));
